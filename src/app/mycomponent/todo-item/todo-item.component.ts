9714ac15dc8bb13b150e39dcb60c2592d1b0ce6a
Hetal <hetalvasaikar1499@gmail.com>
2021-05-26 12:33:19 +0530

    Initial commit

---
+++ src/app/mycomponent/todo-item/todo-item.component.ts
@@ -0,0 +1,29 @@
+import { EmitterVisitorContext } from '@angular/compiler';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/todos';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo: Todo;
+  @Input() i: number;
+  @Output() todoDelete : EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckBox : EventEmitter<Todo> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick(todo:Todo){
+    this.todoDelete.emit(todo);
+    console.log("You have clicked delete button");
+  }
+
+  onCheckboxClick(todo){
+    this.todoCheckBox.emit(todo);
+  }
+
+}
